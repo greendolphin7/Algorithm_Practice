9db18d6137e2e91e8528928483eb0e60c140e7ea
Jay <jawoo.jk@gmail.com>
2021-01-26 16:43:28 +0900

    BOJ 11399

--- 알고리즘/Greedy_Algorithm/11399_ATM.py
+++ 알고리즘/Greedy_Algorithm/11399_ATM.py
@@ -2,12 +2,11 @@ total_person_number = int(input())  # 5
 time_list = list(map(int, input().split(" ")))  # 3 1 4 3 2
 
 que_time = 0
+que_list = []
+time_list = sorted(time_list)
 
-for i in range(len(time_list)):
-    min_time = min(time_list)
-    que_time = que_time + que_time + min_time
+for i in time_list:
+    que_time += i
+    que_list.append(que_time)
 
-    a = time_list.index(min_time)
-    del time_list[a]
-
-print(que_time)  # 미완성
+print(sum(que_list))
