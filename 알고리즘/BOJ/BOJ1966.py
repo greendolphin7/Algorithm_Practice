65baaa2624c33fa55eaed99a5cecd681e3dc1ff1
Jay <jawoo.jk@gmail.com>
2021-03-21 15:59:31 +0900

    프린터 큐

--- 알고리즘/BOJ/BOJ1966.py
+++ 알고리즘/BOJ/BOJ1966.py
@@ -1,23 +1,43 @@
-# 프린터 큐
 T = int(input())
 
-for _ in range(T):
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    que = list(map(int, input().split(' ')))
+    que = list(map(int, input().split()))
 
     for idx, i in enumerate(que):
         que[idx] = (i, idx)
 
-    # que = [(i, idx) for idx, i in enumerate(que)]
-
     count = 0
     while True:
-        if que[0][0] == max(que, key=lambda x: x[0])[0]:
+        priority = que[0][0]
+        if priority == max(que)[0]:
+            print_num = que.pop(0)
             count += 1
-            if que[0][1] == M:
+            if print_num[1] == M:
                 print(count)
                 break
-            else:
-                que.pop(0)
         else:
             que.append(que.pop(0))
+
+
+### lambda 사용한 풀이
+# T = int(input())
+
+# for _ in range(T):
+#     N, M = map(int, input().split())
+#     que = list(map(int, input().split(' ')))
+
+#     for idx, i in enumerate(que):
+#         que[idx] = (i, idx)
+
+#     count = 0
+#     while True:
+#         if que[0][0] == max(que, key=lambda x: x[0])[0]:
+#             count += 1
+#             if que[0][1] == M:
+#                 print(count)
+#                 break
+#             else:
+#                 que.pop(0)
+#         else:
+#             que.append(que.pop(0))
