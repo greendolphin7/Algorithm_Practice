e1ca99c9215dc2f2fe7c2b469432c9d5503268b1
Jay <jawoo.jk@gmail.com>
2021-04-04 20:54:33 +0900

    Binary Search Tree

---
+++ 알고리즘/BOJ/Binary_Search_Tree.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class NodeMgmt:
+    def __init__(self, head):
+        self.head = head
+    
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    # 비교대상 교체
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+
+head = Node(1)
+BST = NodeMgmt(head)
+BST.insert(2)
