2da76f810afddc914c542dd7257fd539914779e3
Jay <jawoo.jk@gmail.com>
2021-04-05 22:20:39 +0900

    Update Binary_Search_Tree.py

--- 알고리즘/BOJ/Binary_Search_Tree.py
+++ 알고리즘/BOJ/Binary_Search_Tree.py
@@ -26,6 +26,24 @@ class NodeMgmt:
                     self.current_node.right = Node(value)
                     break
 
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+        return False
+
 head = Node(1)
 BST = NodeMgmt(head)
 BST.insert(2)
+BST.insert(3)
+BST.insert(0)
+BST.insert(4)
+BST.insert(8)
+
+print(BST.search(8))
+print(BST.search(9))
