e20a5368f5685dc0489de85e4723200ae90f280b
Jay <jawoo.jk@gmail.com>
2021-04-05 22:13:42 +0900

    제로

---
+++ 알고리즘/BOJ/BOJ10773.py
@@ -0,0 +1,15 @@
+# 제로
+
+K = int(input())
+
+stack = []
+
+for i in range(K):
+    num = int(input())
+
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
