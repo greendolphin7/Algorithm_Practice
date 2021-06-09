57c42cf3b5b011074ad95c719fa3f78af5057f92
Jay <jawoo.jk@gmail.com>
2021-06-09 10:40:53 +0900

    구구단

--- 알고리즘/JUNGOL/1291_구구단.py
+++ 알고리즘/JUNGOL/1291_구구단.py
@@ -1,1 +1,33 @@
 # 구구단
+
+def gugudan(s, e):
+    if s > e:
+        for i in range(1, 10):
+            answer = ''
+            for k in range(s, e - 1, -1):
+                if k * i // 10 == 0:
+                    ki = ' ' + str(k * i)
+                else:
+                    ki = str(k * i)
+                answer += '%s * %s = %s   ' %(str(k), str(i), ki)
+            print(answer)
+    else:
+        for i in range(1, 10):
+            answer = ''
+            for k in range(s, e + 1):
+                if k * i // 10 == 0:
+                    ki = ' ' + str(k * i)
+                else:
+                    ki = str(k * i)
+                answer += '%s * %s = %s   ' %(str(k), str(i), ki)
+            print(answer)
+
+
+s, e = map(int, input().split())
+while True:
+    if 2 <= s <= 9 and 2 <= e <= 9:
+        break
+    else:
+        print("INPUT ERROR!")
+        s, e = map(int, input().split())
+gugudan(s, e)
