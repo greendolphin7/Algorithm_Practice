a405169a36f92aa0f749a403f338485ee06037ef
Jay <jawoo.jk@gmail.com>
2021-06-10 09:29:16 +0900

    별찍기17

---
+++ 알고리즘/BOJ/BOJ10992.py
@@ -0,0 +1,9 @@
+# 별 찍기 - 17
+
+N = int(input())
+
+for i in range(1, N + 1):
+    if i == 1 or i == N:
+        print(' ' * (N - i) + '*' * (2 * i - 1))
+    else:
+        print(' ' * (N - i) + '*' + ' ' * (2 * i - 2 - 1) + '*')
